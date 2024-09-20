1da8efe986898dea6330b0bfcd75041b8be0513e
krishnaprasad k <prasadkrishna1189@gamil.com>
2024-09-20 13:29:30 +0530

    env changed

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,8 @@
 // environment.prod.ts
 export const environment = {
     production: true,
-    baseUrl: 'http://localhost:3000/',
-     url:'http://localhost:3000'
+     baseUrl: 'api.wiskandwillow.kpkz.online',
+     url:'api.wiskandwillow.kpkz.online'
+    // baseUrl: 'http://localhost:3000/',
+    //  url:'http://localhost:3000'
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://localhost:3000/',
-    url:'http://localhost:3000'
+    baseUrl: 'api.wiskandwillow.kpkz.online',
+    url:'api.wiskandwillow.kpkz.online'
+    // baseUrl: 'http://localhost:3000/',
+    // url:'http://localhost:3000'
   };
   
